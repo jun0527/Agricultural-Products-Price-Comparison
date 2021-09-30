885582dbcccc497b77f526be496cefadb0395e32
yijun <sandy3068@yahoo.com.tw>
2021-10-01 02:09:19 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,135 @@
+const typeBtnList = document.querySelectorAll('.js-typeBtnArea button');
+const search = document.querySelector('.js-search');
+const searchBtn = document.querySelector('.js-searchBtn');
+const keyWordTxt = document.querySelector('.js-keyWordTxt');
+const resultArea = document.querySelector('.js-resultArea');
+const sortBtn = document.querySelector('.js-filterSortBtn');
+const sortList = document.querySelector('.js-filterSortList');
+const filterSortTxt = document.querySelectorAll('.js-filterSortTxt');
+
+//變數初始值
+let url = 'https://hexschool.github.io/js-filter-data/data.json';
+let typeCode = 'N04';
+let keyWord = '';
+let data = [];
+let resultArr = [];
+let sortListOpen = false;
+
+//種類按鈕切換
+typeBtnList.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    typeBtnList.forEach((item) => {
+      item.setAttribute('class', '');
+    })
+    typeCode = item.getAttribute('data-typeCode');
+    item.setAttribute('class', 'active');
+  })
+})
+//取得價格資料
+function getData() {
+  axios.get(url)
+  .then((res) => {
+    data = res.data;
+    resultArr = data.filter((item) => item['種類代碼'] === typeCode && item['作物名稱'].indexOf(keyWord) !== -1);
+    switch (resultArr.length) {
+      case 0:
+        sortBtn.disabled = true;
+        break;
+      default:
+        sortBtn.disabled = false;
+        break;
+    }
+    renderResult();
+    })
+}
+
+//搜尋關鍵字功能
+searchBtn.addEventListener('click', () => {
+  keyWord = search.value;
+  getData();
+})
+
+//渲染搜尋結果
+function renderResult() {
+  let str = '';
+  switch (resultArr.length) {
+    case 0:
+      str = `<tr>
+      <td class="notSearch" colspan="7">查詢不到當日的交易資訊QQ</td>
+    </tr>`;
+      break;
+    default:
+      resultArr.forEach((item) => {
+        str += `<tr>
+            <td class="fw-bold" width="15%">${item['作物名稱']}</td>
+            <td class="fw-bold" width="15%">${item['市場名稱']}</td>
+            <td width="14%">${item['上價']}</td>
+            <td width="14%">${item['中價']}</td>
+            <td width="14%">${item['下價']}</td>
+            <td width="14%">${item['平均價']}</td>
+            <td width="14%">${item['交易量']}</td>
+          </tr>`
+      })
+      break;
+  }
+  keyWordTxt.innerHTML = `查看「${keyWord}」的比價結果`;
+  resultArea.innerHTML = str;
+}
+//排序篩選下拉選單開關
+sortBtn.addEventListener('click', (e) => {
+  switch (sortListOpen) {
+    case true:
+      sortList.setAttribute('class', 'js-filterSortList d-none');
+      window.removeEventListener('click', closeSortList);
+      break;
+      case false:
+        sortList.setAttribute('class', 'js-filterSortList');
+        window.addEventListener('click', closeSortList);
+        sortList.addEventListener('click', renderSortList);
+        break;
+      }
+      sortListOpen = !sortListOpen;
+    })
+window.addEventListener('click', closeSortList);
+function closeSortList(e) {
+  switch (e.target.closest('.js-filterSortBtn') === null) {
+    case true:
+      switch (sortListOpen) {
+        case true:
+          sortList.setAttribute('class', 'js-filterSortList d-none');
+          sortListOpen = false;
+      }
+      window.removeEventListener('click', closeSortList);
+      break;
+  }
+}
+function renderSortList(e) {
+  let sortStatus = e.target.closest('li').dataset.sort;
+  let sortStatusTxt = e.target.closest('a').textContent;
+  filterSortTxt.forEach((item) => {
+    item.textContent = sortStatusTxt;
+  })
+  sort(sortStatus);
+  renderResult();
+}
+//排序功能
+function sort(status) {
+  switch (status) {
+    //依交易量排序為降幕
+    case '交易量':
+      resultArr.sort((a, b) => {
+        numA = a[status];
+        numB = b[status];
+        return numB - numA;
+      })
+      break;
+    default:
+      //依交易量排序以外的排序為升幕
+      resultArr.sort((a, b) => {
+        numA = a[status];
+        numB = b[status];
+        return numA - numB;
+      })
+      break;
+  }
+}
